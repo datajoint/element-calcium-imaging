1afede01632faab9b9ef4fce4ef45a753f3e7c0a
Kabilar Gunalan <kabilar@vathes.com>
2020-12-16 14:26:25 -0600

    Remove references to `canonical`

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ import sys
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
-Canonical DataJoint pipeline for calcium imaging with CaImAn pipeline for analysis
+DataJoint imaging element for calcium imaging data analyzed with Suite2p and/or CaImAn.
 """
 
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='canonical-imaging',
+    name='elements-imaging',
     version='0.0.1',
-    description="Canonical Imaging Pipeline",
+    description="Calcium imaging DataJoint element",
     long_description=long_description,
     author='DataJoint NEURO',
     author_email='info@vathes.com',
     license='MIT',
-    url='https://github.com/vathes/canonical-imaging',
+    url='https://github.com/datajoint/elements-imaging',
     keywords='neuroscience calcium-imaging science datajoint',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     scripts=[],
