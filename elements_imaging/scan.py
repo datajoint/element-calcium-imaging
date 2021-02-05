796c3da2c4d9c94e1b116bce5792d42e222bb5f6
Thinh Nguyen <thinh@vathes.com>
2021-02-05 10:12:15 -0600

    added support for ScanBox as another acquisition software

--- elements_imaging/scan.py
+++ elements_imaging/scan.py
@@ -3,7 +3,6 @@ ScanImage scans
 """
 
 import datajoint as dj
-import scanreader
 import pathlib
 import importlib
 import inspect
@@ -33,6 +32,10 @@ def activate(scan_schema_name, *, create_schema=True, create_tables=True, linkin
                     Retrieve the list of ScanImage files associated with a given Scan
                     :param scan_key: key of a Scan
                     :return: list of ScanImage files' full file-paths
+                + get_scan_box_files(scan_key: dict) -> list
+                    Retrieve the list of ScanBox files (*.sbx) associated with a given Scan
+                    :param scan_key: key of a Scan
+                    :return: list of ScanBox files' full file-paths
     """
 
     if isinstance(linking_module, str):
@@ -67,7 +70,23 @@ def get_scan_image_files(scan_key: dict) -> list:
     return _linking_module.get_scan_image_files(scan_key)
 
 
+def get_scan_box_files(scan_key: dict) -> list:
+    """
+    Retrieve the list of ScanBox files (*.sbx) associated with a given Scan
+    :param scan_key: key of a Scan
+    :return: list of ScanBox files' full file-paths
+    """
+    return _linking_module.get_scan_box_files(scan_key)
+
+
 # ----------------------------- Table declarations ----------------------
+
+@schema
+class AcquisitionSoftware(dj.Lookup):
+    definition = """  # Name of software used for acquisition of the scans - e.g. ScanImage, ScanBox
+    acq_software: varchar(12)    
+    """
+    contents = zip(['ScanImage', 'ScanBox'])
 
 
 @schema
@@ -88,6 +107,7 @@ class Scan(dj.Manual):
     scan_id: int        
     ---
     -> Equipment  
+    -> AcquisitionSoftware  
     scan_notes='' : varchar(4095)         # free-notes
     """
 
@@ -113,10 +133,10 @@ class ScanInfo(dj.Imported):
     nrois                   : tinyint           # number of ROIs (see scanimage's multi ROI imaging)
     x                       : float             # (um) ScanImage's 0 point in the motor coordinate system
     y                       : float             # (um) ScanImage's 0 point in the motor coordinate system
-    fps                     : float             # (Hz) frames per second
+    fps                     : float             # (Hz) frames per second - Volumetric Scan Rate 
     bidirectional           : boolean           # true = bidirectional scanning
-    usecs_per_line          : float             # microseconds per scan line
-    fill_fraction           : float             # raster scan temporal fill fraction (see scanimage)
+    usecs_per_line=null     : float             # microseconds per scan line
+    fill_fraction=null      : float             # raster scan temporal fill fraction (see scanimage)
     """
 
     class Field(dj.Part):
@@ -131,7 +151,7 @@ class ScanInfo(dj.Imported):
         field_x             : float         # (um) center of field in the motor coordinate system
         field_y             : float         # (um) center of field in the motor coordinate system
         field_z             : float         # (um) relative depth of field
-        delay_image         : longblob      # (ms) delay between the start of the scan and pixels in this field
+        delay_image=null    : longblob      # (ms) delay between the start of the scan and pixels in this field
         """
 
     class ScanFile(dj.Part):
@@ -142,52 +162,95 @@ class ScanInfo(dj.Imported):
 
     def make(self, key):
         """ Read and store some scan meta information."""
-        # Read the scan
-        scan_filenames = get_scan_image_files(key)
-        scan = scanreader.read_scan(scan_filenames)
+        acq_software = (Scan & key).fetch1('acq_software')
 
-        # Insert in ScanInfo
-        self.insert1(dict(key,
-                          nfields=scan.num_fields,
-                          nchannels=scan.num_channels,
-                          nframes=scan.num_frames,
-                          ndepths=scan.num_scanning_depths,
-                          x=scan.motor_position_at_zero[0],
-                          y=scan.motor_position_at_zero[1],
-                          fps=scan.fps,
-                          bidirectional=scan.is_bidirectional,
-                          usecs_per_line=scan.seconds_per_line * 1e6,
-                          fill_fraction=scan.temporal_fill_fraction,
-                          nrois=scan.num_rois if scan.is_multiROI else 0))
+        if acq_software == 'ScanImage':
+            import scanreader
+            # Read the scan
+            scan_filepaths = get_scan_image_files(key)
+            scan = scanreader.read_scan(scan_filepaths)
 
-        # Insert Field(s)
-        x_zero, y_zero, z_zero = scan.motor_position_at_zero  # motor x, y, z at ScanImage's 0
-        if scan.is_multiROI:
-            self.Field.insert([dict(key,
-                                    field_idx=field_id,
-                                    px_height=scan.field_heights[field_id],
-                                    px_width=scan.field_widths[field_id],
-                                    um_height=scan.field_heights_in_microns[field_id],
-                                    um_width=scan.field_widths_in_microns[field_id],
-                                    field_x=x_zero + scan._degrees_to_microns(scan.fields[field_id].x),
-                                    field_y=y_zero + scan._degrees_to_microns(scan.fields[field_id].y),
-                                    field_z=z_zero + scan.fields[field_id].depth,
-                                    delay_image=scan.field_offsets[field_id])
-                               for field_id in range(scan.num_fields)])
+            # Insert in ScanInfo
+            x_zero, y_zero, z_zero = scan.motor_position_at_zero  # motor x, y, z at ScanImage's 0
+            self.insert1(dict(key,
+                              nfields=scan.num_fields,
+                              nchannels=scan.num_channels,
+                              nframes=scan.num_frames,
+                              ndepths=scan.num_scanning_depths,
+                              x=x_zero,
+                              y=y_zero,
+                              fps=scan.fps,
+                              bidirectional=scan.is_bidirectional,
+                              usecs_per_line=scan.seconds_per_line * 1e6,
+                              fill_fraction=scan.temporal_fill_fraction,
+                              nrois=scan.num_rois if scan.is_multiROI else 0))
+
+            # Insert Field(s)
+            if scan.is_multiROI:
+                self.Field.insert([dict(key,
+                                        field_idx=field_id,
+                                        px_height=scan.field_heights[field_id],
+                                        px_width=scan.field_widths[field_id],
+                                        um_height=scan.field_heights_in_microns[field_id],
+                                        um_width=scan.field_widths_in_microns[field_id],
+                                        field_x=x_zero + scan._degrees_to_microns(scan.fields[field_id].x),
+                                        field_y=y_zero + scan._degrees_to_microns(scan.fields[field_id].y),
+                                        field_z=z_zero + scan.fields[field_id].depth,
+                                        delay_image=scan.field_offsets[field_id])
+                                   for field_id in range(scan.num_fields)])
+            else:
+                self.Field.insert([dict(key,
+                                        field_idx=plane_idx,
+                                        px_height=scan.image_height,
+                                        px_width=scan.image_width,
+                                        um_height=getattr(scan, 'image_height_in_microns', None),
+                                        um_width=getattr(scan, 'image_width_in_microns', None),
+                                        field_x=x_zero,
+                                        field_y=y_zero,
+                                        field_z=z_zero + scan.scanning_depths[plane_idx],
+                                        delay_image=scan.field_offsets[plane_idx])
+                                   for plane_idx in range(scan.num_scanning_depths)])
+        elif acq_software == 'ScanBox':
+            import sbxreader
+            # Read the scan
+            scan_filepaths = get_scan_box_files(key)
+            sbx_meta = sbxreader.sbx_get_metadata(scan_filepaths[0])
+            sbx_matinfo = sbxreader.sbx_get_info(scan_filepaths[0])
+            is_multiROI = bool(sbx_matinfo.mesoscope.enabled)  # currently not handling "multiROI" ingestion
+
+            if is_multiROI:
+                raise NotImplementedError('Loading routine not implemented for ScanBox multiROI scan mode')
+
+            # Insert in ScanInfo
+            x_zero, y_zero, z_zero = sbx_meta['stage_pos']  # motor x, y, z at ScanImage's 0
+            self.insert1(dict(key,
+                              nfields=sbx_meta['num_fields'] if is_multiROI else sbx_meta['num_planes'],
+                              nchannels=sbx_meta['num_channels'],
+                              nframes=sbx_meta['num_frames'],
+                              ndepths=sbx_meta['num_planes'],
+                              x=x_zero,
+                              y=y_zero,
+                              fps=getattr(sbx_meta, 'frame_rate', -1),  #TODO - NOT FOUND
+                              bidirectional=sbx_meta == 'bidirectional',
+                              nrois=sbx_meta['num_rois'] if is_multiROI else 0))
+            # Insert Field(s)
+            if not is_multiROI:
+                px_width, px_height = sbx_meta['frame_size']
+                self.Field.insert([dict(key,
+                                        field_idx=plane_idx,
+                                        px_height=px_height,
+                                        px_width=px_width,
+                                        um_height=px_height * sbx_meta['um_per_pixel_y'] if sbx_meta['um_per_pixel_y'] else None,
+                                        um_width=px_width * sbx_meta['um_per_pixel_x'] if sbx_meta['um_per_pixel_x'] else None,
+                                        field_x=x_zero,
+                                        field_y=y_zero,
+                                        field_z=z_zero + sbx_meta['etl_pos'][plane_idx])
+                                   for plane_idx in range(sbx_meta['num_planes'])])
+
         else:
-            self.Field.insert([dict(key,
-                                    field_idx=plane_idx,
-                                    px_height=scan.image_height,
-                                    px_width=scan.image_width,
-                                    um_height=getattr(scan, 'image_height_in_microns', None),
-                                    um_width=getattr(scan, 'image_width_in_microns', None),
-                                    field_x=x_zero,
-                                    field_y=y_zero,
-                                    field_z=z_zero + scan.scanning_depths[plane_idx],
-                                    delay_image=scan.field_offsets[plane_idx])
-                               for plane_idx in range(scan.num_scanning_depths)])
+            raise NotImplementedError(f'Loading routine not implemented for {acq_software} acquisition software')
 
         # Insert file(s)
         root = pathlib.Path(get_imaging_root_data_dir())
-        scan_files = [pathlib.Path(f).relative_to(root).as_posix() for f in scan_filenames]
+        scan_files = [pathlib.Path(f).relative_to(root).as_posix() for f in scan_filepaths]
         self.ScanFile.insert([{**key, 'file_path': f} for f in scan_files])
