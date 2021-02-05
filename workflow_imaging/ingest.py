85d271f4f4c0081851c527287fbc2a86c745e6b2
Thinh Nguyen <thinh@vathes.com>
2021-02-05 10:13:02 -0600

    added support for ScanBox acquisition software

--- workflow_imaging/ingest.py
+++ workflow_imaging/ingest.py
@@ -1,20 +1,17 @@
-import scanreader
 import pathlib
 import csv
 
 from .pipeline import subject, imaging, scan, Session, Equipment
 from .paths import get_imaging_root_data_dir
 
-from elements_imaging.readers import get_scanimage_acq_time, parse_scanimage_header
-
 
 def ingest_subjects():
     # -------------- Insert new "Subject" --------------
     with open('./user_data/subjects.csv', newline='') as f:
-        subjects_dict = list(csv.DictReader(f, delimiter=','))
+        input_subjects = list(csv.DictReader(f, delimiter=','))
 
-    print(f'\n---- Insert {len(subjects_dict)} entry(s) into subject.Subject ----')
-    subject.Subject.insert(subjects_dict, skip_duplicates=True)
+    print(f'\n---- Insert {len(input_subjects)} entry(s) into subject.Subject ----')
+    subject.Subject.insert(input_subjects, skip_duplicates=True)
 
 
 def ingest_sessions():
@@ -22,37 +19,54 @@ def ingest_sessions():
 
     # ---------- Insert new "Session" and "Scan" ---------
     with open('./user_data/sessions.csv', newline='') as f:
-        sessions_dict = list(csv.DictReader(f, delimiter=','))
+        input_sessions = list(csv.DictReader(f, delimiter=','))
 
     # Folder structure: root / subject / session / .tif (raw)
     sessions, scans, scanners = [], [], []
     session_directories, processing_tasks = [], []
 
-    for session in sessions_dict:
+    for session in input_sessions:
         sess_dir = pathlib.Path(session['session_dir'])
-        tiff_filepaths = [fp.as_posix() for fp in sess_dir.glob('*.tif')]
+        # search for either ScanImage or ScanBox files (in that order)
+        for scan_pattern, scan_type in zip(['*.tif', '*.sbx'], ['ScanImage', 'ScanBox']):
+            scan_filepaths = [fp.as_posix() for fp in sess_dir.glob(scan_pattern)]
+            if len(scan_filepaths):
+                acq_software = scan_type
+                break
 
-        try:  # attempt to read .tif as a scanimage file
-            loaded_scan = scanreader.read_scan(tiff_filepaths)
-        except Exception as e:
-            print(f'ScanImage loading error: {tiff_filepaths}\n{str(e)}')
-            loaded_scan = None
+        if acq_software == 'ScanImage':
+            import scanreader
+            from elements_imaging.readers import get_scanimage_acq_time, parse_scanimage_header
+            try:  # attempt to read .tif as a scanimage file
+                loaded_scan = scanreader.read_scan(scan_filepaths)
+                recording_time = get_scanimage_acq_time(loaded_scan)
+                header = parse_scanimage_header(loaded_scan)
+                scanner = header['SI_imagingSystem'].strip('\'')
+            except Exception as e:
+                print(f'ScanImage loading error: {scan_filepaths}\n{str(e)}')
+                continue
+        elif acq_software == 'ScanBox':
+            import sbxreader
+            try:  # attempt to load scanbox
+                sbx_meta = sbxreader.sbx_get_metadata(scan_filepaths[0])
+                sbx_matinfo = sbxreader.sbx_get_info(scan_filepaths[0])
+                recording_time = sbx_meta.get('recording_time', None)  #TODO - NOT FOUND
+                scanner = sbx_meta.get('imaging_system', None)  #TODO - NOT FOUND
+            except Exception as e:
+                print(f'ScanImage loading error: {scan_filepaths}\n{str(e)}')
+                continue
 
-        if loaded_scan is not None:
-            recording_time = get_scanimage_acq_time(loaded_scan)
-            header = parse_scanimage_header(loaded_scan)
-            session_key = {'subject': session['subject'], 'session_datetime': recording_time}
-            scanner = header['SI_imagingSystem'].strip('\'')
-            if session_key not in Session.proj():
-                scanners.append({'scanner': scanner})
-                sessions.append(session_key)
-                scans.append({**session_key, 'scan_id': 0, 'scanner': scanner})
+        session_key = {'subject': session['subject'], 'session_datetime': recording_time}
+        if session_key not in Session.proj():
+            scanners.append({'scanner': scanner})
+            sessions.append(session_key)
+            scans.append({**session_key, 'scan_id': 0, 'scanner': scanner, 'acq_software': acq_software})
 
-                session_directories.append({**session_key, 'session_dir': sess_dir.relative_to(root_data_dir).as_posix()})
-                
-                parameter_set = [int(s) for s in session['paramset'].split(' ')]
-                for param in parameter_set:
-                    processing_tasks.append({**session_key, 'scan_id': 0, 'paramset_idx': param, 'task_mode': 'load'})
+            session_directories.append({**session_key, 'session_dir': sess_dir.relative_to(root_data_dir).as_posix()})
+
+            parameter_set = [int(s) for s in session['paramset'].split(' ')]
+            for param in parameter_set:
+                processing_tasks.append({**session_key, 'scan_id': 0, 'paramset_idx': param, 'task_mode': 'load'})
 
     print(f'\n---- Insert {len(set(val for dic in scanners for val in dic.values()))} entry(s) into experiment.Equipment ----')
     Equipment.insert(scanners, skip_duplicates=True)
--- workflow_imaging/paths.py
+++ workflow_imaging/paths.py
@@ -24,6 +24,23 @@ def get_scan_image_files(scan_key):
         raise FileNotFoundError(f'No tiff file found in {sess_dir}')
 
 
+def get_scan_box_files(scan_key):
+    # Folder structure: root / subject / session / .sbx
+    data_dir = get_imaging_root_data_dir()
+
+    from .pipeline import Session
+    sess_dir = data_dir / (Session.Directory & scan_key).fetch1('session_dir')
+
+    if not sess_dir.exists():
+        raise FileNotFoundError(f'Session directory not found ({sess_dir})')
+
+    sbx_filepaths = [fp.as_posix() for fp in sess_dir.glob('*.sbx')]
+    if sbx_filepaths:
+        return sbx_filepaths
+    else:
+        raise FileNotFoundError(f'No .sbx file found in {sess_dir}')
+
+
 def get_suite2p_dir(processing_task_key):
     # Folder structure: root / subject / session / suite2p / plane / suite2p_ops.npy
 
