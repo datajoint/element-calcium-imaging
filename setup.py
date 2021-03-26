53b066bf9d1726c8f6bb80a346a01345e77e4f4d
Thinh Nguyen <thinh@vathes.com>
2021-03-26 12:38:39 -0500

    add `version.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
+pkg_name = 'element_calcium_imaging'
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
@@ -12,9 +13,12 @@ DataJoint Element for multi-photon calcium imaging data analyzed with Suite2p an
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
+with open(path.join(here, pkg_name, 'version.py')) as f:
+    exec(f.read())
+
 setup(
     name='element-calcium-imaging',
-    version='0.0.1',
+    version=__version__,
     description="Calcium Imaging DataJoint element",
     long_description=long_description,
     author='DataJoint NEURO',
