51882adae3f7d33f601901d885ddedd86bf4a878
Kabilar Gunalan <kabilar@vathes.com>
2021-06-06 23:03:10 -0500

    Update `long_description`

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ pkg_name = 'workflow_calcium_imaging'
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
-# Workflow for calcium imaging data acquired with ScanImage software and analyzed with Suite2p or CaImAn.
+# Workflow for calcium imaging data acquired with ScanImage or Scanbox software and analyzed with Suite2p or CaImAn.
 
 Build a complete imaging workflow using the DataJoint elements
 + [elements-lab](https://github.com/datajoint/element-lab)
