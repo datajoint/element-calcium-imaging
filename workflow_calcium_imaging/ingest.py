24190ff1c8cfdfb568dc8d55e24b77e041e49c47
tdincer <tolgadincer@gmail.com>
2022-01-14 14:38:40 -0600

    element_data_loader -> element_interface

--- workflow_calcium_imaging/ingest.py
+++ workflow_calcium_imaging/ingest.py
@@ -43,7 +43,7 @@ def ingest_sessions(session_csv_path='./user_data/sessions.csv'):
 
         if acq_software == 'ScanImage':
             import scanreader
-            from element_data_loader import scanimage_utils
+            from element_interface import scanimage_utils
             try:  # attempt to read .tif as a scanimage file
                 loaded_scan = scanreader.read_scan(scan_filepaths)
                 recording_time = scanimage_utils.get_scanimage_acq_time(loaded_scan)
