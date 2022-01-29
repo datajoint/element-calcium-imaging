990243a78228ec9a0bab99206792111f9a1b209c
tdincer <tolgadincer@gmail.com>
2022-01-29 11:01:49 -0600

    fix find_root_directory & find_full_path imports

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,3 +1,4 @@
+import pytest
 import pathlib
 import sys
 
@@ -32,7 +33,7 @@ def test_ingest_sessions(pipeline, sessions_csv, ingest_sessions):
 
 
 def test_find_valid_full_path(pipeline, sessions_csv):
-    from element_calcium_imaging import find_full_path
+    from element_interface.utils import find_full_path
 
     get_imaging_root_data_dir = pipeline['get_imaging_root_data_dir']
 
@@ -55,7 +56,7 @@ def test_find_valid_full_path(pipeline, sessions_csv):
 
 
 def test_find_root_directory(pipeline, sessions_csv):
-    from element_calcium_imaging import find_root_directory
+    from element_interface.utils import find_root_directory
 
     get_imaging_root_data_dir = pipeline['get_imaging_root_data_dir']
 
