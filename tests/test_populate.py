89774bfee608eeb28dd82b72cfb2c61a5c4c85c3
Tolga Dincer <tolgadincer@gmail.com>
2022-03-16 17:47:40 -0500

    Rebased docker updates
    Co-authored-by: Kabilar Gunalan <kabilar@datajoint.com>
    Co-authored-by: Tolga Dincer <tolga@datajoint.com>
    Update docker/docker-compose-dev.yaml
    Update docker/docker-compose-test.yaml
    Update docker/Dockerfile.test
    Update docker/Dockerfile.dev
    remove github sourced packages
    remove ephys
    Update tests/test_populate.py
    Update tests/__init__.py
    remove event_id; fix ingest_event csv list

--- tests/test_populate.py
+++ tests/test_populate.py
@@ -86,7 +86,7 @@ def test_processing_populate_trigger_suite2p_2D(trigger_processing_suite2p_2D, p
     # fetch('KEY')[0] is intentional to keep the test short. otherwise there are 2 keys.
     key = (scan.ScanInfo * imaging.ProcessingParamSet & "subject='subject1'").fetch("KEY")[0]
     output_dir = (imaging.ProcessingTask & key).fetch1('processing_output_dir')
-    output_dir = imaging.find_full_path(get_imaging_root_data_dir(key), output_dir).as_posix()
+    output_dir = imaging.find_full_path(get_imaging_root_data_dir(), output_dir).as_posix()
 
     Suite2p(output_dir)
 
@@ -118,7 +118,6 @@ def test_motion_correction_populate_suite2p_2D(curations, pipeline, testdata_pat
         'px_width', 'px_height')
     assert ave_img.shape == (img_height, img_width)
 
-@pytest.mark.skip()
 def test_motion_correction_populate_suite2p_3D(curations, pipeline, testdata_paths):
     imaging = pipeline['imaging']
     scan = pipeline['scan']
