42f2e11c5feb62cec79dc1b02928845066c492e2
Tolga Dincer <tolgadincer@gmail.com>
2022-11-03 17:42:34 -0500

    add docstrings

--- workflow_calcium_imaging/paths.py
+++ workflow_calcium_imaging/paths.py
@@ -3,18 +3,30 @@ import pathlib
 
 
 def get_imaging_root_data_dir():
+    """Retrieve imaging root data directory."""
+
     data_dir = dj.config.get("custom", {}).get("imaging_root_data_dir", None)
     return pathlib.Path(data_dir) if data_dir else None
 
 
 def get_scan_image_files(scan_key):
+    """Retrieve the list of ScanImage files associated with a given Scan.
+
+    Args:
+        scan_key (dict): Primary key from Scan.
+
+    Returns:
+        path (list): Absolute path(s) of the scan files.
+
+    Raises:
+        FileNotFoundError: If the session directory or tiff files are not found.
+    """
     # Folder structure: root / subject / session / .tif (raw)
     data_dir = get_imaging_root_data_dir()
 
     from .pipeline import session
 
-    sess_dir = data_dir / (session.SessionDirectory &
-                           scan_key).fetch1("session_dir")
+    sess_dir = data_dir / (session.SessionDirectory & scan_key).fetch1("session_dir")
 
     if not sess_dir.exists():
         raise FileNotFoundError(f"Session directory not found ({sess_dir})")
@@ -27,13 +39,24 @@ def get_scan_image_files(scan_key):
 
 
 def get_scan_box_files(scan_key):
+    """Retrieve the list of Scanbox files associated with a given Scan.
+
+    Args:
+        scan_key (dict): Primary key from Scan.
+
+    Returns:
+        path (list): Absolute path(s) of the scan files.
+
+    Raises:
+        FileNotFoundError: If the session directory or scanbox files are not found.
+    """
+
     # Folder structure: root / subject / session / .sbx
     data_dir = get_imaging_root_data_dir()
 
     from .pipeline import session
 
-    sess_dir = data_dir / (session.SessionDirectory &
-                           scan_key).fetch1("session_dir")
+    sess_dir = data_dir / (session.SessionDirectory & scan_key).fetch1("session_dir")
 
     if not sess_dir.exists():
         raise FileNotFoundError(f"Session directory not found ({sess_dir})")
@@ -46,13 +69,23 @@ def get_scan_box_files(scan_key):
 
 
 def get_nd2_files(scan_key):
+    """Retrieve the list of Nikon files associated with a given Scan.
+
+    Args:
+        scan_key (dict): Primary key from Scan.
+
+    Returns:
+        path (list): Absolute path(s) of the scan files.
+
+    Raises:
+        FileNotFoundError: If the session directory or nd2 files are not found.
+    """
     # Folder structure: root / subject / session / .nd2
     data_dir = get_imaging_root_data_dir()
 
     from .pipeline import session
 
-    sess_dir = data_dir / (session.SessionDirectory &
-                           scan_key).fetch1("session_dir")
+    sess_dir = data_dir / (session.SessionDirectory & scan_key).fetch1("session_dir")
 
     if not sess_dir.exists():
         raise FileNotFoundError(f"Session directory not found ({sess_dir})")
@@ -65,13 +98,23 @@ def get_nd2_files(scan_key):
 
 
 def get_prairieview_files(scan_key):
+    """Retrieve the list of Prairieview files associated with a given Scan.
+
+    Args:
+        scan_key (dict): Primary key from Scan.
+
+    Returns:
+        path (list): Absolute path(s) of the scan files.
+
+    Raises:
+        FileNotFoundError: If the session directory or tiff files are not found.
+    """
     # Folder structure: root / subject / session / .tif
     data_dir = get_imaging_root_data_dir()
 
     from .pipeline import session
 
-    sess_dir = data_dir / (session.SessionDirectory &
-                           scan_key).fetch1("session_dir")
+    sess_dir = data_dir / (session.SessionDirectory & scan_key).fetch1("session_dir")
 
     if not sess_dir.exists():
         raise FileNotFoundError(f"Session directory not found ({sess_dir})")
