4628aed6a53205da1721b7f50e91e8de1aff65e0
Tolga Dincer <tolgadincer@gmail.com>
2022-11-03 16:04:13 -0500

    add docstring to imaging_report

--- element_calcium_imaging/imaging_report.py
+++ element_calcium_imaging/imaging_report.py
@@ -9,18 +9,17 @@ imaging = None
 def activate(
     schema_name, imaging_schema_name, *, create_schema=True, create_tables=True
 ):
-    """
-    activate(schema_name, *, create_schema=True, create_tables=True)
-        :param schema_name: schema name on the database server to activate the
+    """Activate this schema.
+
+    Args:
+        schema_name (str): Schema name on the database server to activate the
             `imaging_report` schema
-        :param imaging_schema_name: schema name of the activated imaging element for
+        imaging_schema_name (str): Schema name of the activated imaging element for
             which this imaging_report schema will be downstream from
-        :param create_schema: when True (default), create schema in the database if it
-            does not yet exist.
-        :param create_tables: when True (default), create tables in the database if they
-            do not yet exist.
-    (The "activation" of this imaging_report module should be evoked by one of the
-        imaging modules only)
+        create_schema: When True (default), create schema in the database if it does not
+            yet exist.
+        create_tables: When True (default), create tables in the database if they do not
+            yet exist.
     """
     global imaging
     imaging = dj.create_virtual_module("imaging", imaging_schema_name)
@@ -35,6 +34,14 @@ def activate(
 
 @schema
 class ScanLevelReport(dj.Computed):
+    """Scan level report with figures.
+
+    Attributes:
+        imaging.Segmentation (foreign key): Primary key from imaging.Segmentation.
+        cell_overlayed_image (longblob): Plotly figure object showing the segmented
+            cells on the average image.
+    """
+
     definition = """
     -> imaging.Segmentation
     ---
@@ -42,12 +49,22 @@ class ScanLevelReport(dj.Computed):
     """
 
     def make(self, key):
+        """Compute and ingest the plotly figure objects."""
+
         image_fig = cell_plot.plot_cell_overlayed_image(imaging, key)
         self.insert1({**key, "cell_overlayed_image": image_fig.to_json()})
 
 
 @schema
 class TraceReport(dj.Computed):
+    """Figures of traces.
+
+    Attributes:
+        imaging.Segmentation.Mask (foreign key): Primary key from
+            imaging.Segmentation.Mask.
+        cell_traces (longblob): Plotly figure object showing the cell traces.
+    """
+
     definition = """
     -> imaging.Segmentation.Mask
     ---
@@ -55,5 +72,7 @@ class TraceReport(dj.Computed):
     """
 
     def make(self, key):
+        """Compute and ingest the plotly figure objects."""
+
         trace_fig = cell_plot.plot_cell_traces(imaging, key)
         self.insert1({**key, "cell_traces": trace_fig.to_json()})
