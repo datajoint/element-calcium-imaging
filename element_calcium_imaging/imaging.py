8031b5c45de70e0808e74110033a22c96a2d9913
Tolga Dincer <tolgadincer@gmail.com>
2022-11-09 10:06:18 -0600

    apply docstring changes to imaging_no_curation.py

--- element_calcium_imaging/imaging.py
+++ element_calcium_imaging/imaging.py
@@ -631,8 +631,7 @@ class MotionCorrection(dj.Imported):
         """
 
     def make(self, key):
-        """Populate MotionCorrection with results parsed from analysis outputs
-        """
+        """Populate MotionCorrection with results parsed from analysis outputs"""
 
         method, imaging_dataset = get_loader_result(key, Curation)
 
