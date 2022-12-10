c7aea6f589c14641f650b59d7aa8adf623312e49
Tolga Dincer <tolgadincer@gmail.com>
2022-12-10 11:13:05 -0600

    some clarifications in the docstrings

--- element_calcium_imaging/utils.py
+++ element_calcium_imaging/utils.py
@@ -3,10 +3,14 @@ import numpy as np
 
 def calculate_dff(fs, q=5):
     """
-    fs: np.array(trace)
-        fluorescence trace
-    q: float
-        quantile (e.g. 5)
+    Calculates df/f.
+
+    Args:
+        fs (np.array): Fluorescence trace.
+        q (float): Quantile (e.g. 5)
+
+    Returns:
+        dff (np.array): df over f.
     """
     f0 = np.percentile(fs, q, -1)
     return (fs - f0) / f0
@@ -14,18 +18,15 @@ def calculate_dff(fs, q=5):
 
 def combine_trials(fissa_output, file="result", comp=0):
     """
-    Inputs
-    ------
-    fissa_output: npz object
-        fissa output
-    file: str
-        file in the npz object
-    comp: int
-        signal component. 0 is the cell signal whereas the rest is the background signals
-    Output
-    ------
-    traces: np.array
-        traces for each cell [cell, time]
+    Fissa stores the traces in a splitted manner. This function combines the results.
+
+    Args:
+        fissa_output (npz object): fissa output
+        file (str): file in the npz object.
+        comp (int): signal component. 0 is the cell signal whereas the rest is the background signals.
+
+    Returns:
+        traces (np.array): traces for each cell [cell_id, time].
     """
 
     ntrials = fissa_output[file].shape[1]  # number of imaging (e.g. tiff) files
@@ -39,12 +40,21 @@ def combine_trials(fissa_output, file="result", comp=0):
     return np.array(traces)
 
 
-def calculate_zscore(dff, n_quiet_frames=153):
+def calculate_zscore(trace, n_quiet_frames=153):
     """
-    Client defines the baseline from the first 10 seconds, which corresponds to the first 153 frames.
-    n_quiet_frames: 10s / (65 ms/frames) = 152.43 ~ 153 frames
-    Z_dff0(t) = (dff0(t) - mean(dff0_quiet)) / std(dff0_quiet)
+    Calculate z score using a given quiet activity period.
+
+    Note: The baseline is defined from the scan's first 10 s, which translates to
+        the first 153 frames given the fps = 65 ms / frames.
+        n_quiet_frames: 10s / (65 ms/frames) = 152.43 ~ 153.
+
+    Args:
+        trace (np.array): Trace whose z score to be calculated.
+        n_quiet_frames (int): Number of the frames starting from 0 to define a quite period.
+
+    Returns:
+        z score (np.array): z score of the given trace.
     """
-    mean_dff_quiet = dff[:n_quiet_frames].mean()
-    std_dff_quiet = dff[:n_quiet_frames].std()
-    return (dff - mean_dff_quiet) / std_dff_quiet
+    mean_dff_quiet = trace[:n_quiet_frames].mean()
+    std_dff_quiet = trace[:n_quiet_frames].std()
+    return (trace - mean_dff_quiet) / std_dff_quiet
