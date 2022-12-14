9ff7b9698931cc5bc7c270db1a35ee0fab8408c1
Tolga Dincer <tolgadincer@gmail.com>
2022-12-14 14:25:49 -0600

    make mask_zpix nullable

--- element_calcium_imaging/imaging.py
+++ element_calcium_imaging/imaging.py
@@ -1073,7 +1073,7 @@ class Segmentation(dj.Computed):
         mask_center_z   : int       # center z coordinate in pixel
         mask_xpix       : longblob  # x coordinates in pixels
         mask_ypix       : longblob  # y coordinates in pixels      
-        mask_zpix       : longblob  # z coordinates in pixels        
+        mask_zpix=null  : longblob  # z coordinates in pixels        
         mask_weights    : longblob  # weights of the mask at the indices above
         """
 
