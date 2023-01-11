b5d9d52dff8d081c318db39ba0aae714b2d48b09
tdincer <tolgadincer@gmail.com>
2023-01-11 14:13:23 -0600

    caiman and suite2p installation through pip

--- setup.py
+++ setup.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+import subprocess
 from os import path
+from setuptools import setup, find_packages
 
-pkg_name = next(p for p in find_packages() if '.' not in p)
+
+pkg_name = next(p for p in find_packages() if "." not in p)
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), 'r') as f:
+with open(path.join(here, "README.md"), "r") as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt')) as f:
+with open(path.join(here, "requirements.txt")) as f:
     requirements = f.read().splitlines()
 
-with open(path.join(here, pkg_name, 'version.py')) as f:
+with open(path.join(here, pkg_name, "version.py")) as f:
     exec(f.read())
 
+# Prerequisite of caiman installation to run its setup.py
+subprocess.call(["pip", "install", "numpy", "Cython"])
+
+extras_require = {
+    "suite2p": ["suite2p @ git+https://github.com/datajoint-company/suite2p.git"],
+    "caiman": ["caiman @ git+https://github.com/datajoint-company/CaImAn.git"],
+}
+
 setup(
-    name=pkg_name.replace('_', '-'),
+    name=pkg_name.replace("_", "-"),
     version=__version__,
     description="Calcium Imaging DataJoint element",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='DataJoint',
-    author_email='info@datajoint.com',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    author="DataJoint",
+    author_email="info@datajoint.com",
+    license="MIT",
     url=f'https://github.com/datajoint/{pkg_name.replace("_", "-")}',
-    keywords='neuroscience calcium-imaging science datajoint',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    keywords="neuroscience calcium-imaging science datajoint",
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     scripts=[],
     install_requires=requirements,
+    extras_require=extras_require,
 )
