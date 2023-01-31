812802531b5f90cb2802abcecf34e7e1dc114a45
tdincer <tolgadincer@gmail.com>
2023-01-31 17:34:47 -0500

    fix back

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import subprocess
 from os import path
 from setuptools import setup, find_packages
 
@@ -16,20 +15,6 @@ with open(path.join(here, "requirements.txt")) as f:
 with open(path.join(here, pkg_name, "version.py")) as f:
     exec(f.read())
 
-# Prerequisite of caiman installation to run its setup.py
-subprocess.call(["pip", "install", "numpy", "Cython"])
-
-extras_require = {
-    "suite2p": "suite2p @ git+https://github.com/datajoint-company/suite2p.git",
-    "caiman": "caiman @ git+https://github.com/datajoint-company/CaImAn.git",
-    "readers": [
-        "nd2==0.1.6",
-        "tifffile==2021.11.2",
-        "sbxreader==0.1.6.post1",
-        "scanreader @ git+https://github.com/atlab/scanreader.git",
-    ],
-}
-
 setup(
     name=pkg_name.replace("_", "-"),
     version=__version__,
@@ -44,5 +29,4 @@ setup(
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     scripts=[],
     install_requires=requirements,
-    extras_require=extras_require,
 )
