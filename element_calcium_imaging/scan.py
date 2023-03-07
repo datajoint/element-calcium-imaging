841a8ae4f4badb5a02c4e84afa5fd92de5a5f8b4
Tolga Dincer <tolgadincer@gmail.com>
2023-03-07 08:55:12 -0600

    fix syntax error & blackify

--- element_calcium_imaging/scan.py
+++ element_calcium_imaging/scan.py
@@ -695,7 +695,8 @@ class ScanQualityMetrics(dj.Computed):
             nd2_dims = {k: i for i, k in enumerate(nd2_file.sizes)}
 
             valid_dimensions = "TZCYX"
-            assert set(nd2_dims) <= set(valid_dimensions) 
+            assert set(nd2_dims) <= set(
+                valid_dimensions
             ), f"Unknown dimensions {set(nd2_dims)-set(valid_dimensions)} in file {get_nd2_files(key)[0]}."
 
             # Sort the dimensions in the order of TZCYX, skipping the missing ones.
