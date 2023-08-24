a82f54b9d4199f47fcde16ded2feb4db9b7b4ed9
kushalbakshi <kushalbakshi@gmail.com>
2023-08-24 12:47:25 -0500

    Add Neuropil and Deconvolve traces to nwb

--- element_calcium_imaging/export/nwb/nwb.py
+++ element_calcium_imaging/export/nwb/nwb.py
@@ -22,56 +22,7 @@ from ... import imaging, scan
 from ...scan import get_imaging_root_data_dir, get_image_files
 
 
-def add_scan_to_nwb(session_key, nwbfile):
-    from math import nan
-
-    try:
-        scan_key = (scan.Scan & session_key).fetch1("KEY")
-    except DataJointError:
-        raise NotImplementedError(
-            "Exporting more than one scan per session to NWB is not supported yet."
-        )
-
-    scanner_name, scan_notes = (scan.Scan & scan_key).fetch1("scanner", "scan_notes")
-    device = nwbfile.create_device(
-        name=scanner_name if scanner_name is not None else "TwoPhotonMicroscope",
-        description="Two photon microscope",
-        manufacturer="Microscope manufacturer",
-    )
-
-    no_channels, frame_rate = (scan.ScanInfo & scan_key).fetch1("nchannels", "fps")
-
-    field_keys = (scan.ScanInfo.Field & scan_key).fetch("KEY")
-
-    for channel in range(no_channels):
-        optical_channel = OpticalChannel(
-            name=f"OpticalChannel{channel+1}",
-            description=f"Optical channel number {channel+1}",
-            emission_lambda=nan,
-        )
-
-        for field_key in field_keys:
-            field_no = (scan.ScanInfo.Field & field_key).fetch1("field_idx")
-            imaging_plane = nwbfile.create_imaging_plane(
-                name=f"ImagingPlane{field_no+1}",
-                optical_channel=optical_channel,
-                imaging_rate=frame_rate,
-                description=scan_notes
-                if scan_notes != ""
-                else f"Imaging plane for channel {channel+1}",
-                device=device,
-                excitation_lambda=nan,
-                indicator="unknown",
-                location="unknown",
-                grid_spacing=(0.01, 0.01),
-                grid_spacing_unit="meters",
-                origin_coords=[1.0, 2.0, 3.0],
-                origin_coords_unit="meters",
-            )
-    return imaging_plane
-
-
-def rawdata_to_nwb(session_key, output_directory, nwb_path):
+def create_raw_data_nwbfile(session_key, output_directory, nwb_path):
 
     acquisition_software = (scan.Scan & session_key).fetch1("acq_software")
     session_paramset_key = (imaging.ProcessingTask & session_key).fetch1("paramset_idx")
@@ -137,16 +88,65 @@ def rawdata_to_nwb(session_key, output_directory, nwb_path):
 
     metadata = converter.get_metadata()
     metadata["NWBFile"].update(session_description="DataJoint Session")
-    converter.run_conversion(nwbfile_path=(nwb_path / "nwbfile"), metadata=metadata)
+    converter.run_conversion(nwbfile_path=(nwb_path / f'{session_key["subject"]}_nwbfile'), metadata=metadata)
 
 
-def add_image_series_to_nwb(session_key, plane):
+def add_scan_to_nwb(session_key, nwbfile):
+    from math import nan
+
+    try:
+        scan_key = (scan.Scan & session_key).fetch1("KEY")
+    except DataJointError:
+        raise NotImplementedError(
+            "Exporting more than one scan per session to NWB is not supported yet."
+        )
+
+    scanner_name, scan_notes = (scan.Scan & scan_key).fetch1("scanner", "scan_notes")
+    device = nwbfile.create_device(
+        name=scanner_name if scanner_name is not None else "TwoPhotonMicroscope",
+        description="Two photon microscope",
+        manufacturer="Microscope manufacturer",
+    )
+
+    no_channels, frame_rate = (scan.ScanInfo & scan_key).fetch1("nchannels", "fps")
+
+    field_keys = (scan.ScanInfo.Field & scan_key).fetch("KEY")
+
+    for channel in range(no_channels):
+        optical_channel = OpticalChannel(
+            name=f"OpticalChannel{channel+1}",
+            description=f"Optical channel number {channel+1}",
+            emission_lambda=nan,
+        )
+
+        for field_key in field_keys:
+            field_no = (scan.ScanInfo.Field & field_key).fetch1("field_idx")
+            imaging_plane = nwbfile.create_imaging_plane(
+                name=f"ImagingPlane{field_no+1}",
+                optical_channel=optical_channel,
+                imaging_rate=frame_rate,
+                description=scan_notes
+                if scan_notes != ""
+                else f"Imaging plane for channel {channel+1}",
+                device=device,
+                excitation_lambda=nan,
+                indicator="unknown",
+                location="unknown",
+                grid_spacing=(0.01, 0.01),
+                grid_spacing_unit="meters",
+                origin_coords=[1.0, 2.0, 3.0],
+                origin_coords_unit="meters",
+            )
+    return imaging_plane
+
+
+def add_image_series_to_nwb(session_key, imaging_plane):
     imaging_files = (scan.ScanInfo.ScanFile & session_key).fetch("file_path")
     two_p_series = TwoPhotonSeries(
         name="TwoPhotonSeries",
         dimension=(scan.ScanInfo.Field & session_key).fetch1("px_height", "px_width"),
         external_file=imaging_files,
-        imaging_plane=plane,
+        imaging_plane=imaging_plane,
         starting_frame=[0],
         format="external",
         starting_time=0.0,
@@ -160,7 +160,7 @@ def add_motion_correction_to_nwb(session_key, nwbfile):
     )
 
 
-def add_segmentation_data_to_nwb(session_key, nwbfile, plane):
+def add_segmentation_data_to_nwb(session_key, nwbfile, imaging_plane):
     ophys_module = nwbfile.create_processing_module(
         name="ophys", description="optical physiology processed data"
     )
@@ -168,7 +168,7 @@ def add_segmentation_data_to_nwb(session_key, nwbfile, plane):
     ps = img_seg.create_plane_segmentation(
         name="PlaneSegmentation",
         description="output from segmenting",
-        imaging_plane=plane,
+        imaging_plane=imaging_plane,
     )
     ophys_module.add(img_seg)
 
@@ -190,7 +190,7 @@ def add_segmentation_data_to_nwb(session_key, nwbfile, plane):
     channels = (scan.ScanInfo & session_key).fetch1("nchannels")
     for channel in range(channels):
         roi_resp_series = RoiResponseSeries(
-            name=f"RoiFluorescenceSeries{channel}",
+            name=f"Fluorescence_{channel}",
             data=np.stack(
                 (
                     imaging.Fluorescence.Trace
@@ -202,14 +202,40 @@ def add_segmentation_data_to_nwb(session_key, nwbfile, plane):
             unit="a.u.",
             rate=(scan.ScanInfo & session_key).fetch1("fps"),
         )
-    fl = Fluorescence(roi_response_series=roi_resp_series)
+        neuorpil_series = RoiResponseSeries(
+            name=f"Neuropil_{channel}",
+            data=np.stack(
+                (
+                    imaging.Fluorescence.Trace
+                    & session_key
+                    & f"fluo_channel='{channel}'"
+                ).fetch("neuropil_fluorescence")
+            ).T,
+            rois=rt_region,
+            unit="a.u.",
+            rate=(scan.ScanInfo & session_key).fetch1("fps"),
+        )
+        deconvolved_series = RoiResponseSeries(
+            name=f"Deconvolved_{channel}",
+            data=np.stack(
+                (
+                    imaging.Activity.Trace
+                    & session_key
+                    & f"fluo_channel='{channel}'"
+                ).fetch("activity_trace")
+            ).T,
+            rois=rt_region,
+            unit="a.u.",
+            rate=(scan.ScanInfo & session_key).fetch1("fps"),
+        )
+    fl = Fluorescence(roi_response_series=[roi_resp_series, neuorpil_series, deconvolved_series])
     ophys_module.add(fl)
 
 
 def imaging_session_to_nwb(
     session_key,
     save_path=None,
-    raw=False,
+    include_raw_data=False,
     processed_data_source="database" or "filesystem",
     lab_key=None,
     project_key=None,
@@ -229,9 +255,9 @@ def imaging_session_to_nwb(
         )
         save_path = find_full_path(get_imaging_root_data_dir(), output_relative_dir)
 
-    if raw:
-        rawdata_to_nwb(session_key, output_directory=output_dir, nwb_path=save_path)
-        with NWBHDF5IO((save_path / "nwbfile"), mode="r+") as io:
+    if include_raw_data:
+        create_raw_data_nwbfile(session_key, output_directory=output_dir, nwb_path=save_path)
+        with NWBHDF5IO((save_path / f'{session_key["subject"]}_nwbfile'), mode="r+") as io:
             nwb_file = io.read()
             if session_to_nwb:
                 nwb_file = session_to_nwb(
@@ -262,8 +288,8 @@ def imaging_session_to_nwb(
             nwb_file = NWBFile(**nwbfile_kwargs)
 
         imaging_plane = add_scan_to_nwb(session_key, nwbfile=nwb_file)
-        add_image_series_to_nwb(session_key, plane=imaging_plane)
-        add_segmentation_data_to_nwb(session_key, nwbfile=nwb_file, plane=imaging_plane)
+        add_image_series_to_nwb(session_key, imaging_plane=imaging_plane)
+        add_segmentation_data_to_nwb(session_key, nwbfile=nwb_file, imaging_plane=imaging_plane)
 
         return nwb_file
 
