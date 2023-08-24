bfc1125eb2fa8c356778c1a1fc8374577ec23476
kushalbakshi <kushalbakshi@gmail.com>
2023-08-24 15:58:01 -0500

    Update `key_source` for `Processing`

--- element_calcium_imaging/imaging_no_curation.py
+++ element_calcium_imaging/imaging_no_curation.py
@@ -366,7 +366,7 @@ class Processing(dj.Computed):
             * ProcessingTask.aggr(PerPlaneProcessing.proj(), finished_task_count="count(*)")
             & "task_count = finished_task_count"
         )
-        return ks & per_plane_proc
+        return ks - per_plane_proc
 
     def make(self, key):
         """Execute the calcium imaging analysis defined by the ProcessingTask."""
